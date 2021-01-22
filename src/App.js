abfb333f40351dae6a3bfa91033055838659f8a6
Muhammad Osama <mosamarafique@gmail.com>
2021-01-22 22:44:08 +0500

    first Coursera commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="dark" margin="ml auto" justify="space-evenly">
+          <div className="container">
+            <NavbarBrand href="/">Bon Soir Eliot!! </NavbarBrand>
+            </div>
+          
+            
+        </Navbar>
+      </div>
+    );
+  }
 }
-
 export default App;
